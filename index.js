52486b8de4f3e1b7f1c0edf62ad12f86f1721496
ko kristin <koberlander115@gmail.com>
2019-02-05 16:44:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,16 +9,54 @@ function setCart(c) {
   return cart;
 }
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
 function addToCart(item) {
- // write your code here
+ var price = getRandomIntInclusive(1, 100);
+ var added = {'itemName': item, 'itemPrice': price };
+ cart.push(added);
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  for (var i = 0; i < cart.length; i++){
+    
+    if (cart.length===1){
+      return `In your cart, you have ${cart[0]['itemName']} at $${cart[0]['itemPrice']}.`;
+        
+      
+      
+    } else if (cart.length === 2){
+           return `In your cart, you have ${cart[0]['itemName']} at $${cart[0]['itemPrice']}, and ${cart[1]['itemName']} at $${cart[1]['itemPrice']}.`;
+        
+      
+      
+    } else if (cart.length >= 3){
+          for(var n = 0; n < cart.length; n++){
+           if(cart.length === 3){
+            return `In your cart, you have ${cart[0]['itemName']} at $${cart[0]['itemPrice']}, ${cart[1]['itemName']} at $${cart[1]['itemPrice']}, and ${cart[2]['itemName']} at $${cart[2]['itemPrice']}.`
+           } else {
+             var inCart = String(cart).push();
+             return inCart;
+           }
+          }
+          
+          
+     } else {
+        
+      }  
+  } return "Your shopping cart is empty.";
 }
 
-function total() {
-  // write your code here
+function total(){
+var cartTotal = 0;  
+  for (var i = 0; i < cart.length; i++){
+  cartTotal += cart[i].itemPrice;
+} return cartTotal;
 }
 
 function removeFromCart(item) {
